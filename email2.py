441324723afd1a02d131219974f59a980a4b8556
kimdypm@gmail.com <kimdypm@gmail.com>
2025-07-14 11:49:27 +0900

    222

--- email2.py
+++ email2.py
@@ -1,7 +1,18 @@
-# (선택적) 이메일 구독자 추가 코드
-response = sns.subscribe(
-    TopicArn=TOPIC_ARN,
-    Protocol='email',
-    Endpoint='kimdy@gmail.com'  # 수신할 이메일 주소
-)
-print("구독 확인을 이메일로 보냈습니다. 메일을 열어 Confirm 버튼을 클릭하세요.")
+import boto3
+
+sns = boto3.client('sns', region_name='ap-northeast-2')
+
+TOPIC_ARN = 'arn:aws:sns:ap-northeast-2:086015456585:my-sns-topic'   # 실제 ARN으로 수정
+email_list = [
+    'kimdy@gmail.com',
+    'info@edumgt.co.kr',
+    'sangsanq@naver.com'
+]
+
+for email in email_list:
+    response = sns.subscribe(
+        TopicArn=TOPIC_ARN,
+        Protocol='email',
+        Endpoint=email
+    )
+    print(f"{email} → 구독 요청 전송됨. 수신자 이메일에서 확인 필요.")
