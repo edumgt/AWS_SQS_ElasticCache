2a43933a88aedd382126d293f5d86b9d4c4e316c
kimdypm@gmail.com <kimdypm@gmail.com>
2025-07-14 11:40:23 +0900

    2222

--- email2.py
+++ email2.py
@@ -2,6 +2,6 @@
 response = sns.subscribe(
     TopicArn=TOPIC_ARN,
     Protocol='email',
-    Endpoint='example@example.com'  # 수신할 이메일 주소
+    Endpoint='kimdy@gmail.com'  # 수신할 이메일 주소
 )
 print("구독 확인을 이메일로 보냈습니다. 메일을 열어 Confirm 버튼을 클릭하세요.")
